276cab84268dbf51c542f34c76b0f8a7bfa9bf7c
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-09-02 10:28:26 +0900

    라우터 추가, 템플릿엔진 연동

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
+const nunjucks = require("nunjucks");
 const {ExpressPeerServer} = require("peer");
 
 dotenv.config();
@@ -17,58 +18,80 @@ const fs = require("fs");
 const {sequelize} = require("./models");
 const socketServer = require("./socket");
 const util = require("./util/common");
+
 const IndexRouter = require("./router/IndexRouter");
 const LoginRouter = require("./router/LoginRouter");
 const ProfileRouter = require("./router/ProfileRouter");
 const RoomRouter = require("./router/RoomRouter");
 const RoomInfoRouter = require("./router/RoomInfoRouter");
 const SettingRouter = require("./router/SettingRouter");
+const SearchRouter = require("./router/SearchRouter");
 
 const app = express();
 
-sequelize.sync({force:false}).then(()=>{
+sequelize.sync({force: false}).then(() => {
     console.log("연결성공");
-}).catch(r=>{
-    console.log("연결오류 "+r);
+}).catch(r => {
+    console.log("연결오류 " + r);
 });
 
-
-app.set("PORT",process.env.PORT||8080);
-
+// 포트 설정
+app.set("PORT", process.env.PORT || 8080);
+// 템플릿 엔진 설정
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+// post 요청 처리
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(morgan(process.env.MORGAN_SETTING||"dev"))
+app.use(express.urlencoded({extended: true}));
+// 요청 로그 콘솔창에 띄워주는 라이브러리
+app.use(morgan(process.env.MORGAN_SETTING || "dev"))
+// 쿠키 파서
 app.use(cookieParser(process.env.COOKIE_KEY))
+// 세션
 const sessionMiddleware = expressSession({
-    cookie:{
-        secret:process.env.COOKIE_KEY,
-        httpOnly:true
+    secret: process.env.COOKIE_KEY,
+    cookie: {
+        httpOnly: true,
+        secure: false,
     },
-    name:'connect.sid'
+    name: 'connect.sid'
 });
 app.use(sessionMiddleware);
-app.use(cors(
-    {
-        origin:true,
-        credentials: true,
-    },
-));
+// cors 세팅
+const corsConfig = cors({
+    origin: true,
+    credentials: true,
+})
+app.use(corsConfig);
+app.options("*", corsConfig);
+
+// 정적파일 로드
+app.use(express.static(path.join(__dirname,"public","css")));
+app.use(express.static(path.join(__dirname,"public","js")));
+app.use(express.static(path.join(__dirname,"public","img")));
+
+// 라우터
 app.use(IndexRouter);
 app.use(LoginRouter);
 app.use(ProfileRouter);
 app.use(RoomRouter);
 app.use(RoomInfoRouter);
 app.use(SettingRouter);
+app.use(SearchRouter);
 
 
-app.use((req,res,next)=>{
-
-    res.json(util.convertToJson(404,"Not Found","요청한 페이지를 찾을수 없습니다."));
+//404 NotFound 처리 미들웨어
+app.use((req, res, next) => {
+    res.json(util.convertToJson(404, "Not Found", "요청한 페이지를 찾을수 없습니다."));
 });
 
-app.use((err,req,res,next)=>{
+// 오류처리 미들웨어
+app.use((err, req, res, next) => {
     console.log(err);
-    res.json(util.convertToJson(400,"Error",err.message));
+    res.json(util.convertToJson(400, "Error", err.message));
 });
 
 /*
@@ -77,13 +100,16 @@ server.listen(app.get("PORT"),()=>{
     console.log("server is open ",app.get("PORT"));
 });
 */
+// 서버 listen aws nginx letscript 옵션으로 배포시 https
 const server = http.createServer(app);
-server.listen(process.env.HTTP_PORT,()=>{
+server.listen(process.env.HTTP_PORT, () => {
     console.log("http server is open");
-})
+});
+// peer 서버
 const peerServer = ExpressPeerServer(server, {
     debug: true,
 });
 app.use("/peerjs", peerServer);
-socketServer(server,app);
+// 소켓 서버
+socketServer(server, app);
 
--- backend/socket.js
+++ backend/socket.js
@@ -2,7 +2,16 @@ const socketIO = require("socket.io");
 
 module.exports = (server,app)=>{
     const io = socketIO(server,{
-        path:"/socket.io"
-    })
+        origin:"*:*",
+
+    });
+    io.on("connection",(socket)=>{
+        console.log("연결됨")
+        console.log(socket.id)
+        io.emit("join","접속함");
+        socket.on("message",(msg)=>{
+            console.log(msg)
+        });
+    });
 
 }
