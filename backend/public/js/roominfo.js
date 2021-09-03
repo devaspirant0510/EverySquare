6ea0d1e02c101a7416209ec3b5618569ebfe8851
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-09-03 21:25:58 +0900

    socket 연결,카메라 끄기,마이크끼기 등 옵션 추가

---
+++ backend/public/js/roominfo.js
@@ -0,0 +1,141 @@
+const myLocalVideo = document.querySelector("#my-video");
+const videoGrid = document.querySelector("#root-video-grid");
+const btnHandleCam = document.querySelector("#btn-handle-cam");
+const btnHandleMic = document.querySelector("#btn-handle-mic");
+const btnHandlePresent = document.querySelector("#btn-handle-present");
+const btnHandleMessage = document.querySelector("#btn-handle-message");
+
+const videoChatOptions = {
+    video:true,
+    audio:true,
+    screenShare:false,
+    isShowChat:false,
+}
+const ROOM_KEY = window.location.pathname.split("/")[2]
+const socket = io.connect("http://127.0.0.1:8081/",{
+    path:"/socket.io",
+    transport:['websocket']
+});
+console.log(socket)
+const peer = new Peer();
+const peers = {};
+console.log(peer)
+
+const options = {
+    video:true,
+    audio:true,
+}
+
+//const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+// 내 로컬 비디오 연결
+navigator.mediaDevices.getUserMedia(options).then(stream =>{
+    console.log(stream);
+    const video = document.createElement("video");
+    addVideoStream(video,stream,true);
+    peer.on("call",(call)=>{
+        console.log("my video call")
+        call.answer(stream);
+        const video = document.createElement("video");
+        call.on("stream",(userVideoStream)=>{
+            console.log("my video stream")
+            addVideoStream(video,userVideoStream);
+        });
+        call.on("close",()=>{
+            console.log("my video close")
+            video.remove();
+            videoGrid.removeChild(video);
+        });
+    });
+    socket.emit("mesg","adfs");
+    console.log("socket on join 대기");
+    socket.on("message",(msg)=>{
+        console.log(msg)
+    });
+    // 유저 입장할때마다 peerId 넘겨줘서 stream 연결후 video 화면에 추가
+    socket.on("user-connect",(nickname,peerId)=>{
+        console.log("socket on join",nickname,peerId)
+        connectUser(peerId,stream);
+    });
+    handleVideoConfigure(stream);
+}).catch(err=>{
+    console.log(err);
+});
+
+socket.on("user-disconnect",(id)=>{
+    console.log("disconnect",id);
+    if(peers[id]){
+        console.log(peers[id]);
+        peers[id].close();
+    }
+})
+
+peer.on("open",id=>{
+    console.log("my video open peer on",id);
+    console.log(ROOM_KEY,id,USER_INFO)
+    socket.emit("join-room",ROOM_KEY,id,USER_INFO);
+});
+
+// 유저가 입장할때 peerjs 를통해 유저 스티림을 가져옴
+function connectUser(peerId,stream){
+    const call = peer.call(peerId,stream);
+    console.log(`connect to user ${call}`);
+    const video = document.createElement("video");
+    call.on("stream",(userVideoStream)=>{
+        console.log("user stream")
+        addVideoStream(video,userVideoStream,false);
+    });
+    call.on("close",()=>{
+        console.log("user close")
+        video.remove();
+    });
+    console.log(peerId,call)
+    peers[peerId] = call;
+}
+
+// 스트림객체를 넘겨 화면에 비디오를 띄워줌
+function addVideoStream(video,stream,isMe){
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata",()=>{
+        video.play();
+    });
+    videoGrid.append(video);
+
+}
+function handleVideoConfigure(stream){
+    // 카메라 끄기, 켜기 버튼을 눌렀을때
+    btnHandleCam.addEventListener("click",(evt)=>{
+        const btnCamOn = btnHandleCam.children[0];
+        const btnCamOff = btnHandleCam.children[1];
+        // 카메라가 켜진상태라면
+        console.log(evt.currentTarget.value)
+        if(videoChatOptions.video){
+            // 옵션에서 카메라를 꺼진 상태로 만들고
+            videoChatOptions.video = false;
+            // stream 객체에서 enabled 를 false
+            stream.getVideoTracks()[0].enabled = false;
+            btnCamOn.style.display = "none";
+            btnCamOff.style.display = "block";
+        }else{
+            videoChatOptions.video = true;
+            stream.getVideoTracks()[0].enabled = true;
+            btnCamOn.style.display = "block";
+            btnCamOff.style.display = "none";
+        }
+    });
+    btnHandleMic.addEventListener("click",(evt)=>{
+        const btnMicOn = btnHandleMic.children[0];
+        const btnMicOff = btnHandleMic.children[1];
+        if(videoChatOptions.audio){
+            videoChatOptions.audio = false;
+            stream.getAudioTracks()[0].enabled = false;
+            btnMicOn.style.display = "none";
+            btnMicOff.style.display = "block";
+        }else{
+            videoChatOptions.audio = true;
+            stream.getAudioTracks()[0].enabled = true;
+            btnMicOn.style.display = "block";
+            btnMicOff.style.display = "none";
+        }
+    })
+}
--- backend/socket.js
+++ backend/socket.js
@@ -2,16 +2,24 @@ const socketIO = require("socket.io");
 
 module.exports = (server,app)=>{
     const io = socketIO(server,{
-        origin:"*:*",
-
+        path:"/socket.io"
     });
-    io.on("connection",(socket)=>{
-        console.log("연결됨")
-        console.log(socket.id)
-        io.emit("join","접속함");
-        socket.on("message",(msg)=>{
-            console.log(msg)
-        });
+    const room = io.of("/room");
+    io.on("connection",async (socket)=>{
+        console.log("접속")
+        socket.on("join-room",(roomKey,peerId,userInfo)=>{
+            console.log("socket on join-room")
+            console.log(userInfo.nickname)
+            console.log("join-room",roomKey,peerId,userInfo);
+            socket.join(roomKey);
+            socket.to(roomKey).broadcast.emit("user-connect",userInfo.nickname,peerId);
+
+            socket.on("disconnect",()=>{
+                console.log("socket disconnected");
+                console.log(userInfo.nickname)
+                socket.to(roomKey).broadcast.emit("user-disconnect",userInfo.id);
+            });
+        })
     });
 
 }
